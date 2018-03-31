7aa0a05bdf0fa4c0ddc3999f50b60ca50dd04d4d
eklemen <ejklemen@gmail.com>
2018-03-31 18:17:21 -0400

    update event working

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -7,6 +7,32 @@ const User = require('../models').User;
 const eventAttrs = ['uuid', 'venue', 'eventDate', 'title'];
 const userAttrs = ['uuid', 'email', 'phone', 'igUsername', 'igFullName', 'profilePicture', 'businessName'];
 const clientAttrs = ['uuid', 'firstName', 'lastName', 'company', 'phone'];
+
+function getEventById(id, key='id') {
+  return Event
+    .findOne({
+      where: {[key]: id},
+      attributes: eventAttrs,
+      include: [
+        {
+          model: User,
+          as: 'creator',
+          attributes: userAttrs
+        }
+      ]
+    })
+    .then(event => {
+      if(!event) {
+        return res.status(404).send({
+          message: 'ERROR: Event not found.',
+          status: 404
+        })
+      }
+      return res.status(200).send(event)
+    })
+    .catch(error => res.status(500).send(error));
+}
+
 module.exports = {
   create(req, res) {
     const {venue, eventDate, title} = req.body;
@@ -85,7 +111,6 @@ module.exports = {
     if('venue' in req.body) updateObj.venue = venue;
     if('eventDate' in req.body) updateObj.eventDate = eventDate;
     if('title' in req.body) updateObj.title = title;
-    console.log('---------\n\r', updateObj);
     return Event
       .update(
         updateObj,
@@ -94,60 +119,39 @@ module.exports = {
             uuid: req.params.uuid
           },
           returning: true,
-          attributes: eventAttrs,
+          plain: true,
           include: [
             {
               model: User,
               as: 'creator',
-              attributes: userAttrs
             }
           ]
         }
       )
       .spread((_rows, event) => {
-        console.log('event.creator------------\n\r', event[0].dataValues);
-        const {uuid, venue, eventDate, title, creator} = event[0].dataValues;
-        return res.status(200).send({uuid, venue, eventDate, title, creator})
+        return Event
+          .findById(event.dataValues.id, {
+            attributes: eventAttrs,
+            include: [
+              {
+                model: User,
+                as: 'creator',
+                attributes: userAttrs
+              }
+            ]
+          })
+          .then(e => {
+            if(!e) {
+              return res.status(404).send({
+                message: 'ERROR: Event not found.',
+                status: 404
+              })
+            }
+            return res.status(200).send(e)
+          })
+          .catch(error => res.status(500).send(error));
       })
       .catch((error) => res.status(400).send(error));
-    // return Event
-    //   .findOne({
-    //     where: {uuid: req.params.uuid},
-    //     attributes: eventAttrs,
-    //     include: [
-    //       {
-    //         model: User,
-    //         as: 'creator',
-    //         attributes: userAttrs
-    //       }
-    //     ]
-    //   })
-    //   .then(event => {
-    //     if (!event) {
-    //       return res.status(404).send({
-    //         message: 'ERROR: Event with this uuid does not exist.',
-    //         status: 404,
-    //       });
-    //     }
-    //     const {dataValues: {venue, eventDate, title}} = event;
-    //     const {body} = req;
-    //     return event
-    //       .update({
-    //         venue: body.venue         || venue,
-    //         eventDate: body.eventDate || eventDate,
-    //         title: body.title         || title,
-    //       })
-    //       .spread((_rows, event) => {
-    //         console.log('event.creator------------\n\r', event.creator);
-    //         const {uuid, venue, eventDate, title, creator} = event;
-    //         return res.status(200).send({uuid, venue, eventDate, title, creator})
-    //       })
-    //       .catch((error) => res.status(400).send(error));
-    //   })
-    //   .catch((error) => res.status(500).send({
-    //     error,
-    //     message: 'ERROR: Internal server error',
-    //   }));
   },
   softDelete(req, res) {
     return Event
